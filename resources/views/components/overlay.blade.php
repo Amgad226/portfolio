3c8602a7c2291ea516a5d3805ce82c2bb5fdbea8
amjad alwattar <amgad.wr.1@gmail.com>
2025-02-02 02:12:35 +0300

    fear: add overlay component in development mode

--- resources/views/components/overlay.blade.php
+++ resources/views/components/overlay.blade.php
@@ -5,18 +5,32 @@
 <style>
     .overlay {
         position: fixed;
+
         top: 0;
         left: 0;
-        width: 100vw;
-        height: 100vh;
-        background-color: rgba(0, 0, 0, 0.94); 
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.95); /* 50% opacity black */
         display: flex;
         justify-content: center;
         align-items: center;
-        color: white;
-        font-size: 2rem;
-        font-weight: bold;
         text-align: center;
-        z-index: 9999; 
+        z-index: 9999; /* Ensures it stays on top */
+        padding: 20px; /* Prevents text from touching the edges on small screens */
     }
+
+    .overlay h1 {
+        position: fixed;
+        align-items: center;
+        align-content: center;
+        justify-content: center;
+        text-align: center;
+
+        color: white;
+        font-size: 2rem; /* Default size */
+        font-weight: bold;
+        max-width: 90%; /* Prevents text overflow */
+    }
+
+ 
 </style>
