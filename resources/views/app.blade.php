443ebcae6c8307f2fadf7d31b71eecdb6d57e0b0
amjad alwattar <amgad.wr.1@gmail.com>
2025-02-05 22:33:12 +0300

    fix: return overlay for prodation stage

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,14 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>My Portfolio</title>
-    <link rel="stylesheet" href="mediaqueries.css" />
+    {{-- <link rel="stylesheet" href="mediaqueries.css" /> --}}
   </head>
   @vite('resources/js/app.js')
   @vite('resources/css/style.css')
   <body>
-    {{-- @if(config("app.env")==="production")
+    @if(config("app.env")==="production")
     <x-overlay />
-    @endif --}}
+    @endif
     <x-nav-bar />
     <x-home />
     <x-footer />
