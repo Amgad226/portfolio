90afa2e685fa1d6e1064fe0a1fdbbba7ba4ea4bc
amjad alwattar <amgad.wr.1@gmail.com>
2025-02-22 02:55:21 +0300

    feat : add visitors tables in db and store new visit on open the home page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\Visitor;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', function (Request $request) {
+
+    Visitor::create(['ip_address'=>$request->ip(),"user_agent"=>$request->userAgent()]);
     return view('app');
 });
