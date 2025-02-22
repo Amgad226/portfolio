cbe00a7d8ef6073ea24ac835c190de5459b2e417
amjad alwattar <amgad.wr.1@gmail.com>
2025-02-22 05:17:31 +0300

    fix: store new visit only if the client not logged in

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (Request $request) {
-
-    Visitor::create(['ip_address'=>$request->ip(),"user_agent"=>$request->userAgent()]);
+    if(!session()->has('authenticated')){
+        Visitor::create(['ip_address'=>$request->ip(),"user_agent"=>$request->userAgent()]);
+    }
     return view('app');
 });
 
