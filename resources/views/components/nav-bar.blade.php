3e8c456dbc6d2828a1758b1ab5ce7ae6f055d178
amjad alwattar <amgad.wr.1@gmail.com>
2025-02-22 04:31:12 +0300

    fix: imprve nav bar style

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,13 +1,18 @@
 <div>
     <!-- Simplicity is an acquired taste. - Katharine Gerould -->
     <nav id="desktop-nav">
-        <div class="logo">Amjad Alwattar</div>
+        <div id="desktop-nav-name" class="logo">Amjad Alwattar</div>
         <x-nav-links/>
     </nav>
 </div>
 
 <style>
-    @media screen and (max-width: 1200px) {
+      @media screen and (max-width: 1550px) {
+        #desktop-nav-name {
+            display: none;
+        }
+    }
+    @media screen and (max-width: 1300px) {
         #desktop-nav {
             display: none;
         }
