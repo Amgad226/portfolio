bc25b7e17a18bee70ab24e1bb73e88a66c61f7f6
amjad alwattar <amgad.wr.1@gmail.com>
2026-04-25 18:34:11 +0300

    refactor contact component to use asset() for image paths

--- resources/views/partials/contact.blade.php
+++ resources/views/partials/contact.blade.php
@@ -4,8 +4,12 @@
        <h1 class="title">Contact Me</h1>
        <div class="contact-info-upper-container">
            <x-contact.info  extra_class="email-icon" title="amgad.wr.1@gmail.com" href="mailto:amgad.wr.1@gmail.com"
-               img="./assets/email.png" />
-           <x-contact.info  title="LinkedIn" href="https://www.linkedin.com/in/amjad-alwattar/" img="/assets/linkedin.png" />
-           <x-contact.info  title="+963982578083" img="/assets/phone.png" />
+               :img="asset('assets/email.png')" />
+           <x-contact.info 
+            title="LinkedIn"
+            href="https://www.linkedin.com/in/amjad-alwattar/"
+            :img="asset('assets/linkedin.png')"
+            />
+           <x-contact.info  title="+963982578083" :img= "asset('assets/phone.png')" />
        </div>
    </section>
